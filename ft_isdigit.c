0b38f67765e705e95c457b56abc66ce10414d31f
makpolatt <184925550+makpolatt@users.noreply.github.com>
2024-10-15 12:36:54 +0300

    Add files via upload

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -6,16 +6,13 @@
 /*   By: makpolat <makpolat@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 11:49:17 by makpolat          #+#    #+#             */
-/*   Updated: 2024/10/15 11:54:31 by makpolat         ###   ########.fr       */
+/*   Updated: 2024/10/15 12:36:34 by makpolat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <ctype.h>
-#include <stdio.h>
-int ft_isdigit(int c)
+int	ft_isdigit(int c)
 {
 	if (c >= 48 && c <= 57)
 		return (1);
 	return (0);
-	
 }
